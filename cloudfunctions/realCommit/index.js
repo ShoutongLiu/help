0730d6c07c8ac86888691d70a4ec396047939773
v_jianxchen <v_jianxchen@tencent.com>
2020-09-25 12:07:48 +0800

    update

--- cloudfunctions/realCommit/index.js
+++ cloudfunctions/realCommit/index.js
@@ -7,18 +7,28 @@ const _ = DB.command
 
 // 云函数入口函数
 exports.main = async (event, context) => {
+  context.code = 0
+  context.errMsg = 'OK'
   const wxContext = cloud.getWXContext()
   await DB.collection('userInfo').where({_openid: wxContext.OPENID}).get().then(res=>{
-    console.log(res.data)
+    context.isReal = res.data.length?true:false
+    
   })
-  await DB.collection('userInfo').add({
-    data:{
-      _openid: wxContext.OPENID,
-      realnameInfo:event.realnameInfo,
-      phone:event.phone
-    }
-  }).then(res=>{})
-  return "提交成功"
+  //用户未实名的话添加进数据库
+  if(!context.isReal){
+    await DB.collection('userInfo').add({
+      data:{
+        _openid: wxContext.OPENID,
+        realnameInfo:event.realnameInfo,
+        phone:event.phone,
+        fileID:event.fileID
+      }
+    }).then(res=>{})
+  }else{
+    context.code = -1001
+    context.errMsg = '提交失败，该用户已经实名'
+  }
+  return {code:context.code,errMsg:context.errMsg}
 
   // return {
   //   event,
